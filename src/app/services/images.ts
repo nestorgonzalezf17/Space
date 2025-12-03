e53a9cc37d16f3349f5d6e31b29c968d1329433e
RealLion <nestorgonzalezf17@gmail.com>
2025-12-03 01:29:09 -0500

    Final

--- src/app/services/images.ts
+++ src/app/services/images.ts
@@ -18,19 +18,19 @@ export class Images {
       id: 1,
       nombre: 'Astronautas en misiones planetarias',
       descripcion : 'Los astronautas exploran otros planetas para estudiar su geología y buscar signos de vida pasada. Estas cuevas pueden ofrecer protección contra la radiación y condiciones extremas en la superficie planetaria.',
-      imagen: '/backgraunds/astronautapro.jpg'
+      imagen: './backgraunds/astronautapro.jpg'
     },
     {
       id: 2,
       nombre: 'Nevulosas',
       descripcion: 'Las nevulosas son nubes de gas y polvo en el espacio, donde nacen nuevas estrellas. Pueden tener formas y colores variados, creando paisajes cósmicos impresionantes.',
-      imagen: '/backgraunds/nevulosa.jpeg'
+      imagen: './backgraunds/nevulosa.jpeg'
     },
     {
       id: 3,
       nombre: 'Astros',
       descripcion: 'Los astros son cuerpos celestes que orbitan planetas, reflejando la luz de su estrella. Pueden tener diversas características, como lunas, anillos y atmósferas.',
-      imagen: '/backgraunds/planetapartido.jpg'
+      imagen: './backgraunds/planetapartido.jpg'
     },
   ]
 
