34218d8dec62a3d29a63decab2e89a67eb134906
RealLion <nestorgonzalezf17@gmail.com>
2025-12-03 20:56:26 -0500

    Contact

--- src/app/services/products.ts
+++ src/app/services/products.ts
@@ -18,42 +18,42 @@ export class ProductsService {
       nombre: 'Traje Espacial Orion',
       descripcion: 'Protección avanzada para misiones en ambientes extremos y sin gravedad.',
       precio: 1200,
-      imagen: '/imgProducts/traje_orion.webp'
+      imagen: './imgProducts/traje_orion.webp'
     },
     {
       id: 2,
       nombre: 'Rayo Láser de Plasma',
       descripcion: 'Herramienta multifuncional para cortar, defenderse y explorar terrenos alienígenas.',
       precio: 850,
-      imagen: '/imgProducts/rayo_laser.jpg'
+      imagen: './imgProducts/rayo_laser.jpg'
     },
     {
       id: 3,
       nombre: 'Ración Galáctica',
       descripcion: 'Alimento comprimido con alto valor nutricional para viajes largos por el espacio.',
       precio: 60,
-      imagen: '/imgProducts/racion.jpg'
+      imagen: './imgProducts/racion.jpg'
     },
     {
       id: 4,
       nombre: 'Sensor de Vida Extraterrestre',
       descripcion: 'Detecta señales biológicas en planetas desconocidos y estructuras abandonadas.',
       precio: 300,
-      imagen: '/imgProducts/sensor.jpg'
+      imagen: './imgProducts/sensor.jpg'
     },
     {
       id: 5,
       nombre: 'Módulo de Propulsión Ionica',
       descripcion: 'Sistema de impulso silencioso y eficiente para naves de exploración.',
       precio: 1500,
-      imagen: '/imgProducts/propulsor.jpg'
+      imagen: './imgProducts/propulsor.jpg'
     },
     {
       id: 6,
       nombre: 'Mapa Estelar Holográfico',
       descripcion: 'Proyección interactiva de constelaciones y rutas interplanetarias.',
       precio: 200,
-      imagen: '/imgProducts/mapa.webp'
+      imagen: './imgProducts/mapa.webp'
     }
   ];
 
